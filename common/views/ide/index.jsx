1a0b662d8ed3464448ecc928404defae463bfed4
Craig Condon <craig.condon@hellosign.com>
2015-04-24 00:04:46 -0700

    update docs

--- common/views/ide/index.jsx
+++ common/views/ide/index.jsx
@@ -1,15 +1,38 @@
 var React     = require("react");
-var AceEditor = require("react-ace");
+
+var NoComp = React.createClass({
+  render: function() {
+    return <div></div>;
+  }
+});
+
+
+var AceEditor = process.browser ? require("react-ace") : NoComp;
+var cx        = require("classnames");
 
 // tomorrow
 module.exports = React.createClass({
+  getInitialState: function() {
+    return {
+
+    }
+  },
+  toggleExpansion: function() {
+    this.setState({
+      expanded: !this.state.expanded
+    });
+  },
   render: function() {
     return (
-      <div className="ide">
+      <div className={cx({
+        ide: true,
+        expanded: this.state.expanded,
+        animated: true
+      })}>
         <ul className="nav">
           <li></li>
           <li></li>
-          <li></li>
+          <li onClick={this.toggleExpansion}></li>
         </ul>
 
         <AceEditor
