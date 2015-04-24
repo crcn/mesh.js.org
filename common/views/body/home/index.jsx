5195d2052e2e4951fec6cee0757ab8ea9c6d5d13
Craig Condon <craig.condon@hellosign.com>
2015-04-23 23:27:34 -0700

    add header

--- common/views/body/home/index.jsx
+++ common/views/body/home/index.jsx
@@ -1,21 +1,23 @@
 var React = require("react");
+var IDE   = require("../../ide");
 
 module.exports = React.createClass({
   render: function() {
     return (
       <div className="container home">
         <div className="row content header">
-          <div className="col-sm-6 tagline">
+          <div className="col-sm-5 tagline">
             <h2>Mesh it all up.</h2>
             <span>A featherlight communication utility for building sophisticated features</span>
           </div>
-          <div className="col-sm-6 hidden-xs">
+          <div className="col-sm-7 hidden-xs">
             <div className="navigation">
               <a href="http://github.com/mojo-js/mesh.js">Download</a>
               <a href="/examples">Examples</a>
               <a href="/docs">Documentation</a>
               <a href="http://github.com/mojo-js/mesh.js">GitHub</a>
             </div>
+            <IDE {...this.props} />
           </div>
         </div>
       </div>
---
+++ common/views/ide/index.jsx
@@ -0,0 +1,15 @@
+var React = require("react");
+
+module.exports = React.createClass({
+  render: function() {
+    return (
+      <div className="ide">
+        <ul className="nav">
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+    );
+  }
+});
