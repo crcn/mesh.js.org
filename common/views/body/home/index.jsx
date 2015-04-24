72596f8b941a3e9e0db57f5742db18b9bb7592b1
Craig Condon <craig.condon@hellosign.com>
2015-04-23 23:32:35 -0700

    update

--- common/views/body/home/index.jsx
+++ common/views/body/home/index.jsx
@@ -5,6 +5,7 @@ module.exports = React.createClass({
   render: function() {
     return (
       <div className="container home">
+
         <div className="row content header">
           <div className="col-sm-5 tagline">
             <h2>Mesh it all up.</h2>
@@ -18,6 +19,16 @@ module.exports = React.createClass({
               <a href="http://github.com/mojo-js/mesh.js">GitHub</a>
             </div>
             <IDE {...this.props} />
+          </div>
+        </div>
+
+        <div className="row content sub-header">
+          <div className="inner">
+            <h2>Your one and only communication layer</h2>
+            <p>
+              Messaging is a complex thing. Mesh gives you the utilities needed to build a sophisticated messaging
+              layer that facilitates <strong>all</strong> remote, or local communication for your application - server-side, or client-side.
+            </p>
           </div>
         </div>
       </div>
