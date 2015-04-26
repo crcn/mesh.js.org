a029d36032b60c0d6e51939f9bc646cd9d68b3ff
Craig Condon <craig.condon@hellosign.com>
2015-04-26 11:51:23 -0700

    use matrix3d

--- examples/starfighter/components/map/entity.jsx
+++ examples/starfighter/components/map/entity.jsx
@@ -4,13 +4,26 @@ var Bullet = require("./bullet");
 var caplet = require("caplet");
 
 module.exports = React.createClass({
+  mixins: [caplet.watchModelsMixin],
   render: function() {
     var e = this.props.entity;
+
+    var r = e.rotation;
+
+    if (r < 0) {
+      r = 360 + r;
+    }
+
+    var r1 = Math.cos(r/180*Math.PI);
+    var r2 = Math.sin(r/180*Math.PI);
+
+    var matrix = [r1, r2, -r2, r1, e.x, e.y];
+
     var s = {
-      top: e.y,
-      left: e.x,
-      transform: 'rotate(' + e.rotation + 'deg)'
+      transform: 'matrix(' + matrix + ')'
     };
+
+
     return <div className="entity" style={s}>{{
       ship   : <Ship entity={e} />,
       bullet : <Bullet entity={e} />
