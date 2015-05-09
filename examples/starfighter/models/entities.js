c556909f3d714875d7e04ef19d843a4aec001f6b
Craig Condon <craig.condon@hellosign.com>
2015-05-08 22:32:43 -0700

    update

--- examples/starfighter/models/entities.js
+++ examples/starfighter/models/entities.js
@@ -2,24 +2,36 @@ var caplet = require("caplet");
 var Entity = require("./entity");
 
 var classes = {
-  ship: require("./ship"),
-  bullet: require("./bullet")
+  ship   : require("./ship"),
+  bullet : require("./bullet")
 };
 
 module.exports = caplet.createCollectionClass({
+
+  /**
+   */
+
   mixins: [
     require("./mixins/collection")
   ],
+
+  /**
+   */
+
   createModel: function(properties) {
     properties.bus   = this.bus;
     properties.space = this.space;
     var clazz = classes[properties.type] || Entity;
     return clazz(properties);
   },
+
+  /**
+   */
+
   add: function(props, onSave) {
     props.cid       = createCID();
     props.timestamp = Date.now();
-    
+
     var entity = this.createModel(props);
 
     // insert immediately
