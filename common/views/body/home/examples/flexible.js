aea2ebead594c9e3cd171c8d707817e8912c128a
Craig Condon <craig.condon@hellosign.com>
2015-05-30 10:16:58 -0700

    update wording

--- common/views/body/home/examples/flexible.js
+++ common/views/body/home/examples/flexible.js
@@ -1,1 +1,26 @@
-var abc;
+var mesh = require("mesh");
+var sift = require("sift");
+var http = require("mesh-http");
+
+var bus = http({ prefix: "/api" });
+
+bus = mesh.accept(
+  sift({ collection: "users", name: "insert" }),
+  mesh.attach(function(operation) {
+    return {
+      pathname: "/createUser",
+      method: "POST"
+      data: operation.data
+    }
+  }, bus)
+);
+
+var user = new UserModel({
+  name: "Billy",
+  bus: mesh.attach({ collection: "users" }, bus)
+});
+
+// POST /api/createUser { name: "Billy" }
+user.insert(function() {
+
+});
--- common/views/body/home/index.jsx
+++ common/views/body/home/index.jsx
@@ -26,7 +26,7 @@ var features = [
   },
   {
     title: "Flexible",
-    desc: "Use mesh in your pre-existing codebase. Mesh is flexible enough to work with even the most complex APIs.",
+    desc: "Use mesh in your pre-existing codebase. It's flexible enough to work with even the most complex APIs.",
     source: fs.readFileSync(__dirname + "/examples/flexible.js", "utf8")
   }
 ]
