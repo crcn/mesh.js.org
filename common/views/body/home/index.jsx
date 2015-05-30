cf4067fd6eae31c75b5908a066ce7f5ec2be0a64
Craig Condon <craig.condon@hellosign.com>
2015-05-29 22:24:03 -0700

    update

--- common/views/body/home/index.jsx
+++ common/views/body/home/index.jsx
@@ -3,6 +3,30 @@ var IDE   = require("../../ide");
 var fs    = require("fs");
 
 
+var features = [
+  {
+    title: "Just a utility",
+    desc : "Mesh was initially created to help normalize how applications interact with data sources, but it’s just a bundle of useful functions. You can easily use mesh however you want to write message handling code."
+  },
+  {
+    title: "Decoupled",
+    desc: "Mesh helps decouple your application from data sources. Easily write code that’s isomorphic, highly testable, and isn’t locked into any particular service vendor."
+  },
+  {
+    title: "Testable",
+    desc: "Write highly testable code that’s as resilient, and decoupled as your application code. No more mocking & stubbing API calls."
+  },
+  {
+    title: "Interoperable",
+    desc: "Easily write code that’s interoperable with other services. Features such as offline-mode, rollbacks, and realtime data are a cinch to build."
+  },
+  {
+    title: "Flexible",
+    desc: "Mesh is flexible enough to rangle even the most complex messaging systems."
+  }
+]
+
+
 module.exports = React.createClass({
   render: function() {
     return (
@@ -51,13 +75,16 @@ module.exports = React.createClass({
         </div>
 
 
-        <div className="row features content">
-          <div className="col-sm-6">
-            <h4>Just a utility</h4>
-            <p>Mesh was initially created to help normalize how applications interact with data sources,
-            but it’s just a bundle of useful functions. You can easily use mesh however you want to write message handling code.</p>
-          </div>
-        </div>
+        {
+          features.map(function(feature) {
+            return <div className="row feature content">
+              <div className="col-sm-6">
+                <div className="title">{feature.title}</div>
+                <p>{feature.desc}</p>
+              </div>
+            </div>;
+          })
+        }
       </div>
     );
   }
