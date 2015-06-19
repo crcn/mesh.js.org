b22a97e8d8af4f0c6cbede328cdc8b989e37dccf
Craig Condon <craig.condon@hellosign.com>
2015-06-18 17:46:20 -0700

    update example

--- src/common/components/body/home/examples/main.js
+++ src/common/components/body/home/examples/main.js
@@ -11,7 +11,10 @@ var bus = storage();
 
 // persist all operations to socket.io & any operations from socket.io
 // back to local storage.
-bus = mesh.parallel(bus, realtime({ channel: "operations" }, bus));
+bus = mesh.parallel(
+  bus,
+  realtime({ channel: "operations" }, bus)
+);
 
 // insert data. Persists to local storage, and gets
 // broadcasted to all connected clients.
