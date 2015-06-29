58fe73e0fa03f16900690e3ca2df3d8d490b2c16
Craig Jefferds <craig@spiceapps.com>
2015-06-29 13:24:57 -0700

    Update main.js

--- src/common/components/body/home/examples/main.js
+++ src/common/components/body/home/examples/main.js
@@ -1,19 +1,19 @@
-var mesh    = require("mesh");
-var storage = require("mesh-local-storage");
+var mesh             = require("mesh");
+var createStorageBus = require("mesh-local-storage");
 
 // use any one of these database adapters. They all handle
 // the same CRUD operations
 // var storage = require("mesh-memory");
 // var storage = require("mesh-loki");
-var realtime  = require("mesh-socket.io");
+var createRealtimeBus = require("mesh-socket.io");
 
-var storageBus = storage();
+var storageBus = createStorageBus();
 
 // persist all operations to socket.io & any operations from socket.io
 // back to local storage.
 var mergedBus = mesh.parallel(
   storageBus,
-  realtime({ channel: "operations" }, storageBus)
+  createRealtimeBus({ channel: "operations" }, storageBus)
 );
 
 // insert data. Persists to local storage, and gets
